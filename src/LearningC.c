038dca562c483bd094ba6943ed8a6927fbf257e2
Raul <raulferrodrigues@gmail.com>
2019-05-21 18:41:54 -0300

    first things

---
+++ src/LearningC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : LearningC.c
+ Author      : Raul Fernandes Rodrigues
+ Version     :
+ Copyright   : memes!
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("%d", EOF); /* prints hello, world */
+	return 0;
+}
